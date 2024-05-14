ba54ea09f0e845a714527dc54e772949374a7c1c
kingxalpha <tosinsirmuel@gmail.com>
2024-05-14 21:50:17 +0100

    system off

---
+++ src/component/LeaderBoard/leader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function leader() {
+  return (
+    <div>
+        <div className="leader">
+            <h2>Rank</h2>
+            <h2>Username</h2>
+            <h2>Level</h2>
+            <h2>Point</h2>
+        </div>
+    </div>
+  )
+}
+
+export default leader
